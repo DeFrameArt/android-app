f5e3779342cf30bf167ebc731a22b1cedba885bd
Ankur Bag <bagankur@gmail.com>
2018-06-07 23:24:57 -0400

    Initial commit

---
+++ app/src/main/java/com/deframe/artapp/ui/DashboardFragment.kt
@@ -0,0 +1,24 @@
+package com.deframe.artapp.ui
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.deframe.artapp.R
+
+
+class DashboardFragment : Fragment() {
+
+    companion object {
+        val TAG: String = DashboardFragment::class.java.simpleName
+        fun newInstance() = DashboardFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        activity?.title = getString(R.string.title_map)
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return view
+    }
+
+}
