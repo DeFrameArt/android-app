d1c20507947a801271e9292e4d295b110b5190e9
shitongs <33163174+shitongs@users.noreply.github.com>
2018-06-13 11:54:01 -0400

    Added comments for Login/ Sign up screens
    Added comments for Login/ Sign up screens

--- app/src/main/java/com/deframe/artapp/ui/LoginActivity.kt
+++ app/src/main/java/com/deframe/artapp/ui/LoginActivity.kt
@@ -8,11 +8,20 @@ import android.widget.Button
 import android.widget.TextView
 import com.deframe.artapp.R
 
+
+
 class LoginActivity : AppCompatActivity() {
+
+    /**
+     *
+     * @param savedInstanceState
+     * @return Unit
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //on click listener for login button
         val loginButton = findViewById<Button>(R.id.btn_login)
         loginButton.setOnClickListener {
             val mainPageIntent = Intent(this, MainActivity::class.java)
@@ -20,6 +29,7 @@ class LoginActivity : AppCompatActivity() {
 
         }
 
+        //on click listener for switching to sign up
         val textSignUp = findViewById<TextView>(R.id.txt_signupSwitch)
         textSignUp.setOnClickListener {
             val signUpPageIntent = Intent(this,RegisterActivity::class.java)
@@ -29,6 +39,8 @@ class LoginActivity : AppCompatActivity() {
 
 
     }
+
+
 
 
 }
--- app/src/main/java/com/deframe/artapp/ui/RegisterActivity.kt
+++ app/src/main/java/com/deframe/artapp/ui/RegisterActivity.kt
@@ -9,10 +9,16 @@ import android.widget.TextView
 import com.deframe.artapp.R
 
 class RegisterActivity : AppCompatActivity() {
+    /**
+     *
+     * @param savedInstanceState
+     * @return Unit
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        //on click listener for register button
         val registerButton = findViewById<Button>(R.id.btn_register)
         registerButton.setOnClickListener {
             val mainPageIntent = Intent(this, MainActivity::class.java)
@@ -20,6 +26,7 @@ class RegisterActivity : AppCompatActivity() {
 
         }
 
+        //on click listener for switching to log in
         val textLogIn = findViewById<TextView>(R.id.txt_loginSwitch)
         textLogIn.setOnClickListener {
             val logInPageIntent = Intent(this,LoginActivity::class.java)
