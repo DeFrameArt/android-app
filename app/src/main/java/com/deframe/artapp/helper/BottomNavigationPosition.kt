90d06a5d880cb4c880d4e6f6e86911706ad2961b
shitongs <33163174+shitongs@users.noreply.github.com>
2018-07-02 16:27:26 -0400

    Added comments to BottomNavigationPosition.kt
    Added comments to BottomNavigationPosition.kt

--- app/src/main/java/com/deframe/artapp/helper/BottomNavigationPosition.kt
+++ app/src/main/java/com/deframe/artapp/helper/BottomNavigationPosition.kt
@@ -6,12 +6,24 @@ import com.deframe.artapp.ui.MapViewFragment
 import com.deframe.artapp.ui.ListViewFragment
 import com.deframe.artapp.ui.ProfileFragment
 
+/**
+ *Enumerates the bottom navigation buttons
+ * @property LIST
+ * @property MAP
+ * @property PROFILE
+ */
 enum class BottomNavigationPosition(val position: Int, val id: Int) {
     LIST(0, R.id.list),
     MAP(1, R.id.dashboard),
     PROFILE(2, R.id.profile);
 }
 
+/**
+ * Assigns id to navigation position
+ *
+ * @param id of the button
+ * @return bottom navigation position according to the id
+ */
 fun findNavigationPositionById(id: Int): BottomNavigationPosition = when (id) {
     BottomNavigationPosition.LIST.id -> BottomNavigationPosition.LIST
     BottomNavigationPosition.MAP.id -> BottomNavigationPosition.MAP
@@ -19,15 +31,24 @@ fun findNavigationPositionById(id: Int): BottomNavigationPosition = when (id) {
     else -> BottomNavigationPosition.LIST
 }
 
+/**
+ * Creates fragment according to selected botton
+ *
+ * @return fragment of corresponding button
+ */
 fun BottomNavigationPosition.createFragment(): Fragment = when (this) {
     BottomNavigationPosition.LIST -> ListViewFragment.newInstance()
     BottomNavigationPosition.MAP -> MapViewFragment.newInstance()
     BottomNavigationPosition.PROFILE -> ProfileFragment.newInstance()
 }
 
+/**
+ * get tag of the button
+ *
+ * @return string tag of the button
+ */
 fun BottomNavigationPosition.getTag(): String = when (this) {
     BottomNavigationPosition.LIST -> ListViewFragment.TAG
     BottomNavigationPosition.MAP -> MapViewFragment.TAG
     BottomNavigationPosition.PROFILE -> ProfileFragment.TAG
 }
-
